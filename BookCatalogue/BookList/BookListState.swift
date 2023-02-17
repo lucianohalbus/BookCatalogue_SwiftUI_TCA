a4020d3644475286d64a917a4a13feb1c2041ef4
Luciano <lpuzer@yahoo.com.br>
2023-02-17 12:05:25 -0300

    view layout updade

--- BookCatalogue/BookList/BookListState.swift
+++ BookCatalogue/BookList/BookListState.swift
@@ -9,6 +9,7 @@ struct BookListState: Equatable {
     @BindableState var rating: Double = 0
     @BindableState var favorite: Bool = false
     @BindableState var author: String = ""
+    @BindableState var bookImage: String = ""
     var bookList: [BookListModel] = []
     
 }
--- BookCatalogue/BookList/BookSearchView.swift
+++ BookCatalogue/BookList/BookSearchView.swift
@@ -8,38 +8,25 @@ struct BookSearchView: View {
     
     var body: some View {
         WithViewStore(store) { viewStore in
-            VStack(alignment: .trailing) {
-                
-                searchTextField
-                
-                bookList
-                
-                
-            }
-            .padding(.horizontal, 5)
-            .frame(maxWidth: .infinity)
-        }
-    }
-    
-    var addButton: some View {
-        WithViewStore(store) { viewStore in
-            VStack {
-                Button( action: {
-                    viewStore.send(.addNewBook)
-                }) {
-                    Image(systemName: "plus.circle.fill")
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                        .padding(.horizontal, 10)
+            ZStack {
+                VStack {
+                    
+                    searchTextField
+                    
+                    bookList
+
                 }
-                
+            }
+            .background {
+                Color("mainBackground")
+                    .ignoresSafeArea()
             }
         }
     }
-    
+
     var searchTextField: some View {
         WithViewStore(store) { viewStore in
-            HStack(spacing: 20) {
+            HStack {
                 TextField("Search a book title", text: viewStore.binding(
                     get: \.title,
                     send: BookListAction.setTitle))
@@ -49,80 +36,71 @@ struct BookSearchView: View {
                 }) {
                     Image(systemName: "magnifyingglass")
                         .resizable()
-                        .frame(width: 30, height: 30)
+                        .frame(width: 20, height: 20)
                         .foregroundColor(Color.gray)
-                    
                 }
             }
             .frame(maxWidth: .infinity)
-            .padding()
+            .frame(height: 50)
+            .padding(.horizontal)
             .overlay(RoundedRectangle(cornerRadius: 20)
                 .strokeBorder(Color.blue, lineWidth: 2))
+            .padding(.horizontal)
         }
     }
     
     var bookList: some View {
         WithViewStore(store) { viewStore in
             List {
-                Section {
-                    ForEach(viewStore.bookList) { item in
-                        VStack {
-                            HStack(alignment: .top) {
-                                VStack {
-                                    AsyncImage(
-                                        url: URL(
-                                            string: item.bookImage
-                                        ),
-                                        content: { image in
-                                            image
-                                                .resizable()
-                                                .scaledToFit()
-                                        },
-                                        placeholder: {
-                                            ProgressView()
-                                        }
-                                    )
+                ForEach(viewStore.bookList) { item in
+                    VStack(alignment: .leading) {
+                        HStack(alignment: .top) {
+                            AsyncImage(
+                                url: URL(
+                                    string: item.bookImage
+                                ),
+                                content: { image in
+                                    image
+                                },
+                                placeholder: {
+                                    ProgressView()
                                 }
-                                
-                                VStack(alignment: .leading) {
-                                    Text(item.title)
-                                        .font(.title3)
-                                        .foregroundColor(.black)
-                                        .lineLimit(3)
-                                    Text(item.author)
-                                        .font(.subheadline)
-                                        .foregroundColor(.black)
-                                        .lineLimit(3)
-                                    Text(item.date)
-                                        .font(.subheadline)
-                                        .foregroundColor(.black)
-                                        .lineLimit(3)
-                                }
-                            }
-                            
-                            Text(item.description)
-                                .foregroundColor(.black)
-                                .font(.headline)
-                                .lineLimit(5)
-                            
-                            HStack {
-                                Text("\(item.rating, specifier: "%.1f")")
+                            )
+
+                            VStack(alignment: .leading) {
+                                Text(item.title)
                                     .font(.title3)
-                                
-                                Spacer()
-                                
-                                Image(systemName: item.favorite ?  "checkmark.square" : "square")
+                                    .foregroundColor(.black)
+                                    .lineLimit(3)
+                                Text(item.author)
+                                    .font(.subheadline)
+                                    .foregroundColor(.black)
+                                    .lineLimit(3)
+                                Text(item.date)
+                                    .font(.subheadline)
+                                    .foregroundColor(.black)
                             }
                         }
-                        .listRowSeparator(.hidden)
+                        
+                        Text(item.description)
+                            .foregroundColor(.black)
+                            .font(.headline)
+                            .lineLimit(5)
+                        
+                        HStack {
+                            Spacer()
+                            Text("\(item.rating, specifier: "%.1f")")
+                                .font(.title3)
+                            
+                            Spacer()
+                            
+                            Image(systemName: item.favorite ?  "checkmark.square" : "square")
+                            Spacer()
+                        }
+                        .padding(.top, 4)
                     }
+                    .padding(.top, 16)
                 }
-                .listRowInsets(EdgeInsets())
-                .frame(maxWidth: .infinity)
-                .padding()
-                .overlay(RoundedRectangle(cornerRadius: 20)
-                    .strokeBorder(Color.blue, lineWidth: 2)
-                )
             }
             .edgesIgnoringSafeArea(.all)
             .listStyle(PlainListStyle())
@@ -136,7 +114,37 @@ struct BookSearchView_Previews: PreviewProvider {
         BookSearchView(
             store: Store(
                 initialState: BookListState(
-                title: ""
+                    bookList: [
+                        BookListModel(
+                            id: "e0yJEAAAQBAJ",
+                            title: "SwiftUI Essentials - iOS 16 Edition",
+                            author: "Neil Smyth",
+                            description: "This book aims to teach the skills necessary to build iOS 16 applications using SwiftUI, Xcode 14, and the Swift 5.7 programming language. Beginning with the basics, this book outlines the steps to set up an iOS development environment, together with an introduction to using Swift Playgrounds to learn and experiment with Swift. The book also includes in-depth chapters introducing the Swift 5.7 programming language, including data types, control flow, functions, object-oriented programming, property wrappers, structured concurrency, and error handling. A guided tour of Xcode in SwiftUI development mode follows an introduction to the key concepts of SwiftUI and project architecture. The book also covers creating custom SwiftUI views and explains how these views are combined to create user interface layouts, including stacks, frames, and forms. Other topics covered include data handling using state properties and observable, state, and environment objects, as are key user interface design concepts such as modifiers, lists, tabbed views, context menus, user interface navigation, and outline groups. The book also covers graphics and chart drawing, user interface animation, view transitions and gesture handling, WidgetKit, document-based apps, Core Data, CloudKit, and SiriKit integration. Chapters also explain how to integrate SwiftUI views into existing UIKit-based projects and integrate UIKit code into SwiftUI. Finally, the book explains how to package up a completed app and upload it to the App Store for publication. Along the way, the topics covered in the book are put into practice through detailed tutorials, the source code for which is also available for download. The aim of this book, therefore, is to teach you the skills to build your own apps for iOS 16 using SwiftUI. Assuming you are ready to download the iOS 16 SDK and Xcode 14 and have an Apple Mac system, you are ready to get started.",
+                            bookImage: "http://books.google.com/books/content?id=e0yJEAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                            rating: 0.0,
+                            favorite: false,
+                            date: "2022-09-12"
+                        ),
+                        BookListModel(
+                            id: "nL7JyQEACAAJ",
+                            title: "SwiftUI for Absolute Beginners",
+                            author: "Jayant Varma",
+                            description: "Dive into the world of developing for all of Apple platforms with SwiftUI, Apple’s new framework that makes writing applications faster and easier with fewer lines of code. This book teaches the basics of SwiftUI to help you write amazing native applications using XCode. For developers already familiar with ReactNative, this book reviews the declarative, state-based DSL that manages the UI and updates it automatically will feel just like what they’re used to. You\'ll see how SwiftUI reduces the number of lines of code required to achieve the same effects by over 60% and provides a much better experience. Like the announcement of Swift in 2014, SwiftUI is expected to fundamentally change the way developing programmers approach coding iPhone and iPad applications. This book examines how SwiftUI lowers the entry barrier for developers to write amazing cross-platform applications for iOS and iPadOS as well as WatchOS, Mac OS, and TVOS. What You\'ll Learn Write code in the new SwiftUI syntax Combine views to arrange them for an application Add gestures and controls to an application Who This Book Is For Anyone who wants to learn to develop apps for the Mac, iPhone, iPad, and Apple Watch using the Swift programming language. No previous programming experience is necessary.",
+                            bookImage: "http://books.google.com/books/content?id=nL7JyQEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+                            rating: 0.0,
+                            favorite: false,
+                            date: "2019-11-13"
+                        ),
+                        BookListModel(
+                            id: "hDgEEAAAQBAJ",
+                            title: "SwiftUI Cookbook",
+                            author: "Giordano Scalzo", description: "SwiftUI is an innovative new framework to build UI for all Apple platforms using Swift. This recipe-based guide covers the new features of SwiftUI 2 introduced on iOS14 and helps you migrate from UIkit with a simple learning curve through practical solutions. Learn how SwiftUI combines with Apple dev tools to build truly cross-platform Apple apps.",
+                            bookImage: "http://books.google.com/books/content?id=hDgEEAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                            rating: 0.0,
+                            favorite: false,
+                            date: "2020-10-19"
+                        )
+                    ]
                 ),
                 reducer: bookReducer,
                 environment: BookListEnvironment()
